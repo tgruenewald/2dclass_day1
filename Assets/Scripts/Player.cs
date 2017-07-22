267ac600b9bed65a272d3eda2eab0733605a4622
tgruenewald <terrygruenewald@gmail.com>
2017-07-21 19:28:17 -0600

    day 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private float move;
+	private bool jump;
+	public float maxSpeed = 3.0f;
+	public float jumpForce = 20f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		move = Input.GetAxis ("Horizontal");
+		jump = Input.GetButtonDown ("Jump") || Input.GetButtonDown ("Vertical");
+		//Debug.Log ("move = " + move);
+	}
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		if (jump) {
+			GetComponent<Rigidbody2D> ().AddForce(new Vector2(0f, jumpForce));
+		}
+	}
+}
