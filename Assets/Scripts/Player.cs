2be5f52e0692096ba67e4e73e594bf473b0c94f2
tgruenewald <terrygruenewald@gmail.com>
2017-07-21 21:05:44 -0600

    jumping ground check

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,21 +7,27 @@ public class Player : MonoBehaviour {
 	private bool jump;
 	public float maxSpeed = 3.0f;
 	public float jumpForce = 20f;
+	public bool grounded = true;
+    public LayerMask whatIsGround;
+	private CircleCollider2D groundCheck;
 	// Use this for initialization
 	void Start () {
-		
+		groundCheck = GetComponent<CircleCollider2D>();
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
+		grounded = groundCheck.IsTouchingLayers (whatIsGround);
+
 		move = Input.GetAxis ("Horizontal");
+
 		jump = Input.GetButtonDown ("Jump") || Input.GetButtonDown ("Vertical");
 		//Debug.Log ("move = " + move);
 	}
 	void FixedUpdate () {
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-		if (jump) {
+		if (grounded && jump) {
 			GetComponent<Rigidbody2D> ().AddForce(new Vector2(0f, jumpForce));
 		}
 	}
